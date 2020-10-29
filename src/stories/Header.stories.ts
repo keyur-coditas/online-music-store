851ffc3ed3eaf9bc5c35aa7f1c056ed4e7b77db2
unknown <keyur.joshi@coditas.com>
2020-10-29 13:20:34 +0530

    added storybook for header component

--- src/stories/Header.stories.ts
+++ src/stories/Header.stories.ts
@@ -2,30 +2,81 @@ import { moduleMetadata } from '@storybook/angular';
 import { CommonModule } from '@angular/common';
 // also exported from '@storybook/angular' if you can deal with breaking changes in 6.1
 import { Story, Meta } from '@storybook/angular/types-6-0';
+import { HeaderComponent } from 'src/app/header/header.component';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
+import { Store, StoreModule } from '@ngrx/store';
+import { Router } from '@angular/router';
+import { ThemeService } from 'src/app/shared/theme.service';
+import { AppService } from 'src/app/shared/app.service';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import '../components';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
-import Button from './button.component';
-import Header from './header.component';
+const storeMock = {
+  dispatch: () => {},
+  pipe: () => {},
+};
+const routerMock = {
+  navigate: () => {},
+};
+const themeMock = {
+  isDarkTheme: () => {},
+  setDarkTheme: () => {},
+  setLightTheme: () => {},
+  setActiveTheme: () => {},
+};
+const appServiceMock = {
+  isAuthenticated: () => {},
+};
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 export default {
-  title: 'Example/Header',
-  component: Header,
+  title: 'Header Component',
+  component: HeaderComponent,
   decorators: [
     moduleMetadata({
-      declarations: [Button],
-      imports: [CommonModule],
+      declarations: [HeaderComponent],
+      imports: [
+        CommonModule,
+        TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            useFactory: HttpLoaderFactory,
+            deps: [HttpClient],
+          },
+          defaultLanguage: 'en',
+        }),
+        StoreModule,
+        HttpClientModule,
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
+      providers: [
+        { provide: Store, useValue: storeMock },
+        { provide: Router, useValue: routerMock }
+      ],
     }),
   ],
 } as Meta;
 
-const Template: Story<Header> = (args: Header) => ({
-  component: Header,
+const Template: Story<HeaderComponent> = (args: HeaderComponent) => ({
+  component: HeaderComponent,
   props: args,
 });
 
-export const LoggedIn = Template.bind({});
-LoggedIn.args = {
-  user: {},
-};
+export const LoggedIn = () => ({
+  component: HeaderComponent,
+  props: {
+    isAuthenticated: () => true,
+  },
+});
 
-export const LoggedOut = Template.bind({});
-LoggedOut.args = {};
+export const LoggedOut = () => ({
+  component: HeaderComponent,
+  props: {
+    isAuthenticated: () => false,
+  },
+});
